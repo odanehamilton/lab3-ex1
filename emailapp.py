359972f915fe91d5e662539de66c6dfa6d5b6160
Odane P. Hamilton <odane.hamilton@gmail.com>
2016-02-18 04:40:19 +0000

    First working instance of script

---
+++ emailapp.py
@@ -0,0 +1,32 @@
+import smtplib
+
+fromaddr = 'odane.hamilton@gmail.com'
+fromname = 'Odane'
+toname = 'George'
+subject = 'Test email'
+msg = 'This is the test message to be sent'
+toaddr  = 'odane.hamilton@hotmail.com'
+message = """From: {} <{}>
+To: {} <{}>
+Subject: {}
+
+{}
+"""
+messagetosend = message.format(
+                             fromname,
+                             fromaddr,
+                             toname,
+                             toaddr,
+                             subject,
+                             msg)
+
+# Credentials (if needed)
+username = 'odane.hamilton@gmail.com'
+password = 'xcncejeiwtjbbqzg'
+
+# The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddr, messagetosend)
+server.quit()
